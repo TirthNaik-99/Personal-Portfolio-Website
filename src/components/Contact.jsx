0670ec2805b63410c9a61d5755ab85428c27f870
TirthNaik-99 <naik.ti@northeastern.edu>
2025-03-14 14:30:05 -0400

    updated the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -121,12 +121,7 @@ const Contact = () => {
             {loading ? "Sending..." : "Send"}
           </button>
 
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
+          
         </form>
       </motion.div>
 
